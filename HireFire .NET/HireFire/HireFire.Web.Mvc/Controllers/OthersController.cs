01d55535076d832034367352235d44665e5c60fc
TANIM <iktanim1995@gmail.com>
2018-05-02 14:36:25 +0600

    test controller added

--- HireFire .NET/HireFire/HireFire.Web.Mvc/Controllers/OthersController.cs
+++ HireFire .NET/HireFire/HireFire.Web.Mvc/Controllers/OthersController.cs
@@ -10,15 +10,6 @@ namespace HireFire.Web.Mvc.Controllers
     public class OthersController : Controller
     {
         // GET: Others
-         IUserAuthenticationService _userAuthenticationService;
-         IMessengerService _messengerService;
-
-
-        public OthersController(IUserAuthenticationService authenticationService, IMessengerService _messengerService)
-        {
-            _userAuthenticationService = authenticationService;
-            this._messengerService = _messengerService;
-        }
         public ActionResult Home()
         {
             return View();
@@ -70,51 +61,7 @@ namespace HireFire.Web.Mvc.Controllers
             return View();
         }
 
-        public void LoginTest()//UserAuthenticationServiceIsValidTest
-        {
-            var x = _userAuthenticationService.IsValid("tanim", "1");
-            Response.Write(x);
-            //Login test successful
-        }
-        public void LastMessageTest()
-        {
-            var x = _messengerService.GetLatestMessageByUesrName("tanim");
-            Response.Write(x.Text);
-        }
-        //public void ContactListCheck()//Version 1
-        //{
-        //    var x = _messengerService.GetAllContactListByUserName("tanim");
-        //    Response.Write("ConversionNum   FromUser    ToUser  Text<br/>");
-        //    foreach (var x1 in x)
-        //    {
-        //        Response.Write(x1.ConversionNumber + "    " + x1.FromUser + "     " + x1.ToUser + "    " + x1.Text + "<br/>");
-        //    }
-        //}
-        public void ContactListCheck()//Version 1
-        {
-            var x = _messengerService.GetAllContactListByUserName("tanim");
-            Response.Write("Contact List<br/>");
-            foreach (var x1 in x)
-            {
-                Response.Write(x1+"<br/>");
-            }
-        }
-
-        public void ShowAllMessageCommunicationWithParticularUserTest()
-        {
-            var x = _messengerService.GetAllMessage("rakib","tanim");
-            Response.Write("ConversionNum   FromUser    ToUser  Text<br/>");
-            foreach (var x1 in x)
-            {
-                Response.Write(x1.ConversionNumber + "    " + x1.FromUser + "     " + x1.ToUser + "    " + x1.Text + "<br/>");
-            }
-        }
-
-        public void InsertMessageTest()
-        {
-            var x = _messengerService.Insert("rakib", "Imail", "owwww");
-            Response.Write(x);
-        }
+        
 
 
 
---
+++ HireFire .NET/HireFire/HireFire.Web.Mvc/Controllers/TestController.cs
@@ -0,0 +1,68 @@
+﻿using Hirefire.Core.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HireFire.Web.Mvc.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        IUserAuthenticationService _userAuthenticationService;
+        IMessengerService _messengerService;
+
+        public TestController(IUserAuthenticationService authenticationService, IMessengerService _messengerService)
+        {
+            _userAuthenticationService = authenticationService;
+            this._messengerService = _messengerService;
+        }
+        public void LoginTest()//UserAuthenticationServiceIsValidTest
+        {
+            var x = _userAuthenticationService.IsValid("tanim", "1");
+            Response.Write(x);
+            //Login test successful
+        }
+        public void LastMessageTest()
+        {
+            var x = _messengerService.GetLatestMessageByUesrName("tanim");
+            Response.Write(x.Text);
+        }
+        //public void ContactListCheck()//Version 1
+        //{
+        //    var x = _messengerService.GetAllContactListByUserName("tanim");
+        //    Response.Write("ConversionNum   FromUser    ToUser  Text<br/>");
+        //    foreach (var x1 in x)
+        //    {
+        //        Response.Write(x1.ConversionNumber + "    " + x1.FromUser + "     " + x1.ToUser + "    " + x1.Text + "<br/>");
+        //    }
+        //}
+        public void ContactListCheck()//Version 1
+        {
+            var x = _messengerService.GetAllContactListByUserName("tanim");
+            Response.Write("Contact List<br/>");
+            foreach (var x1 in x)
+            {
+                Response.Write(x1 + "<br/>");
+            }
+        }
+
+        public void ShowAllMessageCommunicationWithParticularUserTest()
+        {
+            var x = _messengerService.GetAllMessage("rakib", "tanim");
+            Response.Write("ConversionNum   FromUser    ToUser  Text<br/>");
+            foreach (var x1 in x)
+            {
+                Response.Write(x1.ConversionNumber + "    " + x1.FromUser + "     " + x1.ToUser + "    " + x1.Text + "<br/>");
+            }
+        }
+
+        public void InsertMessageTest()
+        {
+            var x = _messengerService.Insert("rakib", "Imail", "owwww");
+            Response.Write(x);
+        }
+
+    }
+}
